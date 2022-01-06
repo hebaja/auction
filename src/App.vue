a4131eaa430318ab4e225eb56a766441cbb562d4
hebaja <hebaja@yahoo.com.br>
2022-01-06 13:39:45 -0300

    fixing rebase 3

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,79 @@
 </template>
 
 <script>
+<<<<<<< HEAD
 import HelloWorld from './components/HelloWorld.vue'
+=======
+import AuctioneerPanel from './components/AuctioneerPanel.vue'
+import GroupPlayerPanel from './components/GroupPlayersPanel.vue'
+import SigninPanel from './components/SigninPanel.vue'
+<<<<<<< HEAD
+import SignoutPanel from './components/SignoutPanel.vue'
+=======
+>>>>>>> e27ef58 (commit for heroku)
+import PlayerPanel from './components/PlayerPanel.vue'
+
+import axios from 'axios';
+
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+
+<<<<<<< HEAD
+<<<<<<< HEAD
+import { getMessaging, getToken } from "firebase/messaging";
+
+
+
+=======
+>>>>>>> 9454283 (commit for heroku)
+=======
+console.log(process.env.VUE_APP_NAME)
+
+>>>>>>> 22589b4 (working on environment variables)
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
+}
+
+initializeApp(firebaseConfig);
+
+
+const messaging = getMessaging();
+getToken(messaging, { vapidKey: 'BLI8Ih_57XFC527KdjoPoQnCXDKU8l7jVD15xkINcUN0ZDL6ppg2m1i9TmfC3PAruwjKzZbsm2Ia3L7zBOMOrmU' }).then((currentToken) => {
+  if (currentToken) {
+    // Send the token to your server and update the UI if necessary
+    // ...
+    console.log(currentToken)
+  } else {
+    // Show permission request UI
+    console.log('No registration token available. Request permission to generate one.');
+    // ...
+  }
+}).catch((err) => {
+  console.log('An error occurred while retrieving token. ', err);
+  // ...
+});
+
+
+
+
+// const messaging = getMessaging(firebaseApp)
+
+// messaging.subscribeToTopic('hebajabackup@gmail.com')
+//   .then((response) => {
+//     // See the MessagingTopicManagementResponse reference documentation
+//     // for the contents of response.
+//     console.log('Successfully subscribed to topic:', response);
+//   })
+//   .catch((error) => {
+//     console.log('Error subscribing to topic:', error);
+//   });
+>>>>>>> ee34edc (working on environment variables)
 
 export default {
   name: 'App',
